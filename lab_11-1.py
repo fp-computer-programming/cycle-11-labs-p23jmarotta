2fa08bcdc0925f3746f921a81d2ba6f6fa0c3ecd
p23jmarotta <p23jmarotta@fairfieldprep.org>
2023-01-30 14:09:17 -0500

    11-1 update

--- lab_11-1.py
+++ lab_11-1.py
@@ -1,9 +1,9 @@
 # Creator JM 1/30/2023
 
-row = {'B202_29': 'Preston',
+row = {'B202_29': 'Preston', # original seating chart
        'B202_27': 'Joe',
        'B202_28': 'Sasser'}
 
-print(row)
+print(row) # print
 
-row['B202_27'] = 'guy who stole my seat'
+row['B202_27'] = 'guy who stole my seat' # update dictionary
