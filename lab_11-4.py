ae61b2801da24174e8b2761041b1061128b75458
p23jmarotta <p23jmarotta@fairfieldprep.org>
2023-02-02 14:51:17 -0500

    done 11-4 and 11-5 wip 11-3

---
+++ lab_11-4.py
@@ -0,0 +1,8 @@
+# Creator JM 2/2/2023
+
+weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+weekends = ('Saturday', 'Sunday')
+
+days_of_the_week = weekdays + weekends  # concatenation
+
+print(days_of_the_week)
